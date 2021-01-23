f44e084a9fd0a47ebb1d4b0fc948053fc82d0874
ben <benjaminyelon@gmail.com>
2021-01-23 17:04:04 +0200

    adding bar and scatter plot. working on better area selector.

--- analysis_functions.py
+++ analysis_functions.py
@@ -37,30 +37,26 @@ def generate_composite_params(df):
     return df
 
 
-def display_hists(listings, y_axis):
-    # for each upper area:
-    for upper_area_name, listings in listings.items():
+def display_hists(listings, x_axis, option, upper_name_column, lower_name_column):
 
-        # display each lower area as a bin
-        fig, ax = plt.subplots(len(listings), 1, figsize=(10, 10))
-        # Plots do not play nice with hebrew text. Reverse the strings.
-        upper_area_name = upper_area_name[::-1]
-        fig.suptitle(upper_area_name, fontsize=16)
-
-        x = 0
-        for area_name, df in listings.items():
-            y_axis = df[y_axis]
-            sns.displot(y_axis, ax=ax[x], kind="kde", rug=True)
-            area_name = area_name[::-1]
-            ax[x].set_title(area_name)
-            x += 1
-
+    if option == 'up':
+        df_1 = pd.DataFrame()
+        for upper_area_name, df in listings.items():
+            df_1 = df_1.append(df)
+        sns.relplot(data=df_1, x=x_axis, y=y_axis, col=upper_name_column, col_wrap=4)
         plt.show()
+
+    elif option == 'down':
+        # for each upper area:
+        for upper_area_name, df in listings.items():
+            # display lower areas as subplots
+            sns.relplot(data=df, x=x_axis, y=y_axis, col=upper_name_column, col_wrap=4)
+            plt.suptitle(upper_area_name)
+            plt.show()
 
     return
 
 
-# TODO: generate standard y-axis scale for better comparison
 def display_distributions(listings, x_axis, option, upper_name_column, lower_name_column):
     if option == 'up':
         df_1 = pd.DataFrame()
@@ -81,62 +77,22 @@ def display_distributions(listings, x_axis, option, upper_name_column, lower_nam
     return
 
 
-def display_scatter_plots(listings, x_axis, y_axis, option):
-    # make single plot
+def display_scatter_plots(listings, x_axis, y_axis, option, upper_name_column, lower_name_column):
+
     if option == 'up':
-        n = len(listings)
-        fig, ax = plt.subplots(n, 1, figsize=(10, n * 5 + 5))
-
-        # for each upper area:
-        x = 0
-        for upper_area_name, low_scopes in listings.values():
-            # lump all lower areas per upper area
-            df = pd.DataFrame()
-            for df_1 in low_scopes.values():
-                df.append(df_1)
-
-        x_axis = df[x_axis]
-        y_axis = df[y_axis]
-        sns.scatterplot(x=x_axis, y=y_axis, data=listings)
-        ax.set(xlabel=x_axis, ylabel=y_axis)
-        ax[x].set_title(upper_area_name)
-        x += 1
-
-        plt.xlabel(x_axis)
-        plt.ylabel(y_axis)
+        df_1 = pd.DataFrame()
+        for upper_area_name, df in listings.items():
+            df_1 = df_1.append(df)
+        sns.relplot(data=df_1, x=x_axis, y=y_axis, col=upper_name_column, col_wrap=4)
         plt.show()
 
     elif option == 'down':
-
         # for each upper area:
-        for upper_area_name, listings in listings.items():
-
-            # display lower areas as subplots:
-            n = len(listings)
-            fig, ax = plt.subplots(n, 1, figsize=(10, n * 5 + 5))
-
-            upper_area_name = upper_area_name[::-1]
-            fig.suptitle(upper_area_name, fontsize=16)
-
-            x = 0
-
-            for area_name, df in listings.items():
-                x_axis = df[x_axis]
-
-                # price = df['price']
-                # average_price = price.mean()
-                # print("average price for ", area_name, ":", average_price)
-
-                sns.scatterplot(x=x_axis, y=y_axis, ax=ax[x])
-
-                area_name = area_name[::-1]
-                ax[x].set_title(area_name, )
-                x += 1
-                plt.xlabel(x_axis)
-                plt.ylabel(y_axis)
+        for upper_area_name, df in listings.items():
+            # display lower areas as subplots
+            sns.relplot(data=df, x=x_axis, y=y_axis, col=upper_name_column, col_wrap=4)
+            plt.suptitle(upper_area_name)
             plt.show()
-
-    return
 
 
 def apartment_search():
